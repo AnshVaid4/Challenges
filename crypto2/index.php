37526e584affd61aa01a101158e9a82a9393dbf8
Ansh Vaid <anshvaid4@gmail.com>
2021-08-16 17:38:48 +0530

    done

--- crypto2/index.php
+++ crypto2/index.php
@@ -10,7 +10,7 @@
 <p>Cobb came across a safe, containing the flag for this challenge. On the safe it was written <b>"Your old friend Satoshi"</b>. As usual Cobb took Somnacin to initiate the 
     dream, and met Satoshi to get the flag.
     Satoshi told that he had locked the flag in that digital safe and the key to open it has some relation with his block number 691604, and also that key 
-    is same as that of <b>hash of the block, if it was mined 5 minutes before from the actual time</b>, with same nonce. Well it's Inception 
+    is same as that of <b>hash of the block, if it was mined 5 minutes before</b>, with same nonce. Well it's Inception 
     and Cobb has decided to find it in his dream, if incase he needs some more hints from Satoshi. You are Cob, can you get the flag before the dream finishes?
 </p>
 <br>
