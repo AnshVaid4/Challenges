31939fc7cade0fdcd9341979e776181348938e57
Ansh Vaid <60707011+AnshVaid4@users.noreply.github.com>
2021-12-29 21:25:34 +0530

    Update fileUploadRev.php

--- fileUploadRev.php
+++ fileUploadRev.php
@@ -2,7 +2,7 @@ GIF89a;
 <?php
 // php-reverse-shell - A Reverse Shell implementation in PHP
 // Copyright (C) 2007 pentestmonkey@pentestmonkey.net
-//
+// also read https://vulp3cula.gitbook.io/hackers-grimoire/exploitation/web-application/file-upload-bypass
 // This tool may be used for legal purposes only.  Users take full responsibility
 // for any actions performed using this tool.  The author accepts no liability
 // for damage caused by this tool.  If these terms are not acceptable to you, then
